585e9088d49d38a9177bcfb7748bee025aa05819
lpffer <1365166687@qq.com>
2019-07-09 09:15:30 +0800

    my

--- electrical/src的副本/services/index.js
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request';
-//精选好物
-export let Goothing = params => {
-    console.log("params...", params)
-    return request.post('/api/open/page/home/list/1.0.0', params);
-}
