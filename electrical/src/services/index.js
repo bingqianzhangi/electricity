de8f610a7156e61db9fe3dd17e8e7a9cd34e122d
unknown <923772138@qq.com>
2019-07-12 07:58:33 +0800

    提交代码

--- electrical/src/services/index.js
+++ electrical/src/services/index.js
@@ -1,12 +1,12 @@
 import request from '@/utils/request';
-//精选好物&轮播图
+// 精选好物&轮播图
 export let Goothing = params => {
     return request.post('/api/open/page/home/list/1.0.0', params);
 }
-//为你精选
-export let products = params => {
-    return request.post('/api/open/page/home/products/1.0.0', params);
-}
+// //为你精选
+// export let products = params => {
+//     return request.post('/api/open/page/home/products/1.0.0', params);
+// }
 //tab切换
 export let Tab = params => {
     return request.post('/api/open/product/category/sub/1', params);
@@ -22,9 +22,9 @@ export let labelquery = params => {
 }
 
 // https://upapi.jinaup.com/api/open/product/category/sub/1
-export let TabList = params => {
-    return request.post('/api/open/product/category/query/1.0.0');
-}
+// export let TabList = params => {
+//     return request.post('/api/open/product/category/query/1.0.0');
+// }
 // https://upapi.jinaup.com/api/open/product/category/productList/1.0.0
 //tab列表
 export let getTab=params=>{
---
+++ electrical/src/services/my.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request';
+//收货地址列表
+export let addressFn = () => {
+    return request.post('/api/open/user/address/list/1.0.0')
+}
+//添加收货地址
+export let addAddressFn = (params) => {
+    // console.log('params...',params)
+    return request.post('/api/open/user/address/addAddress/1.0.0',params)
+}
