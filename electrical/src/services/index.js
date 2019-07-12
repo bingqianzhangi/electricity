224ad08b8f2d2401e1e87beb549a32c570a6034b
unknown <923772138@qq.com>
2019-07-12 08:55:41 +0800

    首页完善

--- electrical/src/services/index.js
+++ electrical/src/services/index.js
@@ -3,10 +3,10 @@ import request from '@/utils/request';
 export let Goothing = params => {
     return request.post('/api/open/page/home/list/1.0.0', params);
 }
-// //为你精选
-// export let products = params => {
-//     return request.post('/api/open/page/home/products/1.0.0', params);
-// }
+//为你精选
+export let products = params => {
+    return request.post('/api/open/page/home/products/1.0.0', params);
+}
 //tab切换
 export let Tab = params => {
     return request.post('/api/open/product/category/sub/1', params);
@@ -22,9 +22,9 @@ export let labelquery = params => {
 }
 
 // https://upapi.jinaup.com/api/open/product/category/sub/1
-// export let TabList = params => {
-//     return request.post('/api/open/product/category/query/1.0.0');
-// }
+export let TabList = params => {
+    return request.post('/api/open/product/category/query/1.0.0');
+}
 // https://upapi.jinaup.com/api/open/product/category/productList/1.0.0
 //tab列表
 export let getTab=params=>{
